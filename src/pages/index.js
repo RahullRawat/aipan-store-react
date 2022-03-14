e54b5a2b80a8c4ad4312b82a759b83b96873a7d0
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-15 02:41:29 +0530

    feat : added export function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,6 @@
 export { Home } from "./Homepage/Home";
+export { Product } from "./Products/Product";
+export { Login } from "./Authentication/Login";
+export { Signup } from "./Authentication/Signup";
+export { Cart } from "./Cart/Cart";
+export { Wishlist } from "./Wishlist/Wishlist";
