3f3c3d56fccb0ae023ec817a34cf68ef1f14eb94
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-15 16:38:35 +0530

    feat : added router path

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import { Navbar } from "./components/index";
 import { Home, Product, Login, Signup, Cart, Wishlist } from "./pages/index";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<Home />
-			{/* <Product /> */}
-			{/* <Login /> */}
-			{/* <Signup /> */}
-			{/* <Cart /> */}
-			{/* <Wishlist /> */}
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/product" element={<Product />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="/wishlist" element={<Wishlist />} />
+			</Routes>
 		</div>
 	);
 }
