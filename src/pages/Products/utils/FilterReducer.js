4b1302b267cd8c1f0e1e41ca5dd196616e6b26c1
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-16 22:25:59 +0530

    feat : filter reducer

---
+++ src/pages/Products/utils/FilterReducer.js
@@ -0,0 +1,80 @@
+export const filterReducer = (state, action) => {
+	switch (action.type) {
+		case "SLIDER_RANGE":
+			return {
+				...state,
+				sliderRange: action.payload,
+			};
+
+		case "PRICE_RANGE": {
+			return {
+				...state,
+				priceRange: action.payload,
+			};
+		}
+
+		// case "CATEGORY": {
+		// 	return {
+		// 		...state,
+		// 		category: action.payload,
+		// 	};
+		// }
+
+		case "PAINTINGS": {
+			return {
+				...state,
+				paintings: !state.paintings,
+			};
+		}
+
+		case "BAGS": {
+			return {
+				...state,
+				bags: !state.bags,
+			};
+		}
+
+		case "DIARY": {
+			return {
+				...state,
+				diary: !state.diary,
+			};
+		}
+
+		case "COASTER": {
+			return {
+				...state,
+				coaster: !state.coaster,
+			};
+		}
+
+		case "RATING":
+			return {
+				...state,
+				rating: action.payload,
+			};
+
+		case "SORT":
+			return {
+				...state,
+				sortBy: action.payload,
+			};
+
+		case "RESET":
+			return {
+				...state,
+				sortBy: null,
+				priceRange: null,
+				rating: null,
+				// category: null,
+				paintings: false,
+				bags: false,
+				diary: false,
+				coaster: false,
+				sliderRange: 4000,
+			};
+
+		default:
+			return state;
+	}
+};
