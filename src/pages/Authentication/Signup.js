c5e3c02027a6c7263cd0fcc8e2ef8adbdc16952e
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-18 21:15:26 +0530

    feat : signup functionality

--- src/pages/Authentication/Signup.js
+++ src/pages/Authentication/Signup.js
@@ -1,41 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 export const Signup = () => {
+	const { firstName, email, password, authDispatch, signUpError, signUp } =
+		useAuth();
+
+	const signUpHandler = (e) => {
+		e.preventDefault();
+		signUp();
+	};
+
 	return (
 		<section className="login-container">
+			{signUpError && <h1 className="">{signUpError}</h1>}
 			<div className="login">
 				<h1 className="login-title md-text">Signup</h1>
-				<form className="login-form sm-text">
+				<form className="login-form sm-text" onSubmit={signUpHandler}>
 					<label htmlFor="name">Name</label>
-					<input id="name" type="text" placeholder="Enter Name" />
-					<label htmlFor="email">Username or Email </label>
+					<input
+						id="firstName"
+						type="text"
+						placeholder="Enter Name"
+						name="firstName"
+						value={firstName}
+						onChange={(e) =>
+							authDispatch({ type: "NAME", payload: e.target.value })
+						}
+						required
+					/>
+
+					<label htmlFor="email">Email </label>
 					<input
 						id="email"
-						name="email"
 						type="email"
 						placeholder="Enter Email"
+						name="email"
+						value={email}
+						onChange={(e) =>
+							authDispatch({ type: "SIGN-UP-EMAIL", payload: e.target.value })
+						}
+						required
 					/>
 
 					<label htmlFor="password">Password </label>
-					<input id="password" type="password" placeholder="Enter password" />
+					<input
+						id="password"
+						type="password"
+						placeholder="Enter password"
+						name="password"
+						value={password}
+						onChange={(e) =>
+							authDispatch({
+								type: "SIGN-UP-PASSWORD",
+								payload: e.target.value,
+							})
+						}
+						required
+					/>
 
-					<label htmlFor="password">Confirm Password </label>
-					<input id="password" type="password" placeholder="Enter password" />
+					<div className="term-condition">
+						<input id="term-condition" type="checkbox" />
+						<label htmlFor="term-condition">
+							I accept all terms & conditions
+						</label>
+					</div>
+
+					<button type="submit" className="btn btn-login">
+						Signup
+					</button>
+
+					<div className="no-account">
+						Already have an account ? <Link to="/login">Login </Link>
+					</div>
 				</form>
-
-				<div className="term-condition">
-					<input id="term-condition" type="checkbox" />
-					<label htmlFor="term-condition">
-						I accept all terms & conditions
-					</label>
-				</div>
-
-				<button className="btn btn-login">Signup</button>
-
-				<div className="no-account">
-					Already have an account ? <Link to="/login">Login </Link>
-				</div>
 			</div>
 		</section>
 	);
