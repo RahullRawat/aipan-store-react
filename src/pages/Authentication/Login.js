e6ed61159d56067540f150c3af1fe8979a72c5a9
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-18 21:14:56 +0530

    feat : login functionality

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,32 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Authentication.css";
+import { useAuth } from "../../context/AuthContext";
 
 export const Login = () => {
+	const { email, password, logInError, authDispatch, logIn } = useAuth();
+
+	const logInHandler = (e) => {
+		e.preventDefault();
+		logIn();
+	};
 	return (
 		<section className="login-container">
+			{logInError && <h1 className="">{logInError}</h1>}
 			<div className="login">
 				<h1 className="login-title md-text">Login</h1>
-				<form className="login-form sm-text">
-					<label htmlFor="email">Username or Email </label>
+				<form className="login-form sm-text" onSubmit={logInHandler}>
+					<label htmlFor="email">Email </label>
 					<input
 						id="email"
 						name="email"
 						type="email"
 						placeholder="Enter Email"
+						value={email}
+						onChange={(e) =>
+							authDispatch({ type: "LOG-IN-EMAIL", payload: e.target.value })
+						}
+						required
 					/>
 
 					<label htmlFor="password">Password </label>
-					<input id="password" type="password" placeholder="Enter password" />
-				</form>
-				<Link to="#" className="forgot-password">
-					Forgot Your Password ?
-				</Link>
-				<button className="btn btn-login">Login</button>
+					<input
+						id="password"
+						type="password"
+						placeholder="Enter password"
+						value={password}
+						onChange={(e) =>
+							authDispatch({ type: "LOG-IN-PASSWORD", payload: e.target.value })
+						}
+						required
+					/>
+					<Link to="#" className="forgot-password">
+						Forgot Your Password ?
+					</Link>
+					<button type="submit" className="btn btn-login">
+						Login
+					</button>
 
-				<div className="no-account">
-					Don't have an account ? <Link to="/signup">Sign Up </Link>
-				</div>
+					<div className="no-account">
+						Don't have an account ? <Link to="/signup">Sign Up </Link>
+					</div>
+				</form>
 			</div>
 		</section>
 	);
