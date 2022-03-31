5b24952fba0a2880de0d7e75a1ad8f49b1ca1056
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-03-31 15:36:20 +0530

    feat : import categoriesCard

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CategoriesCard } from "../../components/CategoriesCard/CategoriesCard";
 import "./Home.css";
 
 export const Home = () => {
@@ -11,61 +12,7 @@ export const Home = () => {
 					<button className="btn btn-primary">Shop Now</button>
 				</Link>
 			</div>
-
-			<section>
-				<h1 className="section-title text-center md-text">
-					Featured <span className="highlight-text md-text">Categories</span>
-				</h1>
-				<div className="featured-categories">
-					<div className="featured-items">
-						<Link to="/product">
-							<img
-								src="https://aipan-store.netlify.app/assets/paintings/painting1.jpg"
-								alt="paintings"
-							/>
-						</Link>
-						<Link to="/product">
-							<button className="btn btn-primary btn-category">
-								Paintings
-							</button>
-						</Link>
-					</div>
-					<div className="featured-items">
-						<Link to="/product">
-							<img
-								src="https://aipan-store.netlify.app/assets/bags/bag1.jpg"
-								alt="bags"
-							/>
-						</Link>
-						<Link to="/product">
-							<button className="btn btn-primary btn-category">Bags</button>
-						</Link>
-					</div>
-					<div className="featured-items">
-						<Link to="/product">
-							<img
-								src="https://aipan-store.netlify.app/assets/diary/diary2.jpg"
-								alt="diary"
-							/>
-						</Link>
-						<Link to="/product">
-							<button className="btn btn-primary btn-category">Diary</button>
-						</Link>
-					</div>
-					<div className="featured-items">
-						<Link to="/product">
-							<img
-								src="https://aipan-store.netlify.app/assets/coaster/coaster1.jpg"
-								alt="coaster"
-							/>
-						</Link>
-						<Link to="/product">
-							<button className="btn btn-primary btn-category">Coaster</button>
-						</Link>
-					</div>
-				</div>
-			</section>
-
+			<CategoriesCard />
 			<section>
 				<h1 className="section-title text-center md-text">
 					Best <span className="highlight-text md-text">Selling</span>
@@ -84,7 +31,7 @@ export const Home = () => {
 							<p className="item-name">Sun Painting</p>
 							<span className="currency">Rs 999.00</span>
 						</div>
-						<Link to="/cart">
+						<Link to="/product">
 							<button className="btn btn-primary btn-category">
 								Add to cart
 							</button>
@@ -102,7 +49,7 @@ export const Home = () => {
 							<p className="item-name">Handmade Diary</p>
 							<span className="currency">Rs 560.00</span>
 						</div>
-						<Link to="/cart">
+						<Link to="/product">
 							<button className="btn btn-primary btn-category">
 								Add to cart
 							</button>
@@ -120,7 +67,7 @@ export const Home = () => {
 							<p className="item-name">Lord Buddha Wall Painting</p>
 							<span className="currency">Rs 999.00</span>
 						</div>
-						<Link to="/cart">
+						<Link to="/product">
 							<button className="btn btn-primary btn-category">
 								Add to cart
 							</button>
@@ -138,7 +85,7 @@ export const Home = () => {
 							<p className="item-name">Handcrafted tea coaster</p>
 							<span className="currency">Rs 740.00</span>
 						</div>
-						<Link to="/cart">
+						<Link to="/product">
 							<button className="btn btn-primary btn-category">
 								Add to cart
 							</button>
