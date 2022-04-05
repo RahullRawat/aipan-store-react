6805354d4daaa3122b5329d77ecfc3e3807af8a7
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-05 11:03:55 +0530

    feat : added mobile filter btn

--- src/pages/Products/components/ProductSearch.js
+++ src/pages/Products/components/ProductSearch.js
@@ -4,8 +4,9 @@ export const ProductSearch = () => {
 	return (
 		<>
 			<div className="product-search">
+				<button className="btn-primary btn-filters">Filters</button>
 				<input type="text" placeholder="Enter Product........" />
-				<button className=" btn-search">Search</button>
+				<i class="fa fa-search btn-search" aria-hidden="true"></i>
 			</div>
 		</>
 	);
