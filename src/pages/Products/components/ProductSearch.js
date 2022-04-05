f897b3e489e07a1dc03eb66b0077cdf5026a389a
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-05 14:05:27 +0530

    feat : added mobile filters

--- src/pages/Products/components/ProductSearch.js
+++ src/pages/Products/components/ProductSearch.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { MobileFilters } from "./MobileFilters/MobileFilters";
 
 export const ProductSearch = () => {
+	const [mobileFilters, setMobileFilters] = useState(false);
+	const showMobileFIlters = () => {
+		setMobileFilters(!mobileFilters);
+	};
 	return (
 		<>
 			<div className="product-search">
-				<button className="btn-primary btn-filters">Filters</button>
+				<button className="btn-filters" onClick={showMobileFIlters}>
+					Filters
+				</button>
 				<input type="text" placeholder="Enter Product........" />
 				<i class="fa fa-search btn-search" aria-hidden="true"></i>
 			</div>
+			<MobileFilters mobileFilters={mobileFilters} />
 		</>
 	);
 };
